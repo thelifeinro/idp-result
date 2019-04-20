eb34b328080cc3e2ef5d886dbac5e603df1c1601
Dan <d_pirvan@yahoo.com>
2019-04-20 23:22:10 +0300

    made worker-repo

---
+++ server.js
@@ -0,0 +1,87 @@
+var express = require('express'),
+    async = require('async'),
+    pg = require("pg"),
+    path = require("path"),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+io.set('transports', ['polling']);
+
+var port = process.env.PORT || 4000;
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('message', { text : 'Welcome!' });
+
+  socket.on('subscribe', function (data) {
+    socket.join(data.channel);
+  });
+});
+
+async.retry(
+  {times: 1000, interval: 1000},
+  function(callback) {
+    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
+      if (err) {
+        console.error("Waiting for db");
+      }
+      callback(err, client);
+    });
+  },
+  function(err, client) {
+    if (err) {
+      return console.error("Giving up");
+    }
+    console.log("Connected to db");
+    getTeme(client);
+  }
+);
+
+function getTeme(client) {
+  client.query('SELECT nume, grupa, tema FROM votes', [], function(err, result) {
+    if (err) {
+      console.error("Error performing query: " + err);
+    } else {
+      var teme = collectTemeFromResult(result);
+      io.sockets.emit("teme", JSON.stringify(teme));
+    }
+
+    setTimeout(function() {getTeme(client) }, 1000);
+  });
+}
+
+function collectTemeFromResult(result) {
+  var temele = [];
+
+  result.rows.forEach(function (row) {
+    var temaObj = { nume : row.nume, grupa : row.grupa, tema : row.tema};
+    temele.push(temaObj);
+  });
+
+  return temele;
+}
+
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+});
+
+server.listen(port, function () {
+  var port = server.address().port;
+  console.log('App running on port ' + port);
+});
---
+++ views/app.js
@@ -0,0 +1,71 @@
+var app = angular.module('catsvsdogs', []);
+var socket = io.connect({transports:['polling']});
+
+
+app.controller('statsCtrl', function($scope){
+  var updateScores = function(){
+    socket.on('teme', function (json) {
+       data = JSON.parse(json);
+       //data is an array of homeworks
+       for(let i = 0; i<data.length; i++){
+          let hw = data[i];
+          let nume = hw.nume;
+          let grupa = hw.grupa;
+          let tema = hw.tema;
+
+          addRow(nume, grupa, tema);
+
+       }
+/*       var a = parseInt(data.a || 0);
+       var b = parseInt(data.b || 0);
+
+       var percentages = getPercentages(a, b);
+
+       $scope.$apply(function () {
+         $scope.aPercent = percentages.a;
+         $scope.bPercent = percentages.b;
+         $scope.total = a + b;
+       });*/
+    });
+  };
+
+  var init = function(){
+    document.body.style.opacity=1;
+    updateScores();
+  };
+  socket.on('message',function(data){
+    init();
+  });
+});
+
+function addRow(nume , grupa, tema)
+{
+         tabBody=document.getElementById("tabel");
+         row=document.createElement("tr");
+         cell1 = document.createElement("td");
+         cell2 = document.createElement("td");
+         cell3 = document.createElement("td");
+         textnode1=document.createTextNode(nume);
+         textnode2=document.createTextNode(grupa);
+         textnode3=document.createTextNode(tema);
+         cell1.appendChild(textnode1);
+         cell2.appendChild(textnode2);
+         cell3.appendChild(textnode3);
+         row.appendChild(cell1);
+         row.appendChild(cell2);
+         row.appendChild(cell3);
+         tabBody.appendChild(row);
+}
+
+function getPercentages(a, b) {
+  var result = {};
+
+  if (a + b > 0) {
+    result.a = Math.round(a / (a + b) * 100);
+    result.b = 100 - result.a;
+  } else {
+    result.a = result.b = 50;
+  }
+
+  return result;
+}
